3463d87b8bbdfe9ca2b49d90066bc37dfa6572f1
null <oneour59@gmail.com>
2025-03-24 22:51:50 +0900

    test commit

---
+++ src/main/java/hu/bookmarker/BookmarkerApplication.java
@@ -0,0 +1,13 @@
+package hu.bookmarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bookmarker/BookmarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bookmarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
