204ae488d2a195cc73b338e4de01a53867599eb3
null <greatnull2@gmail.com>
2025-07-14 16:46:21 +0900

    CORS 설정 수정

--- src/main/java/hu/bookmarker/config/WebConfig.java
+++ src/main/java/hu/bookmarker/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "https://bkmk-front.vercel.app")
+                    .allowedOriginPatterns("http://localhost:3000", "https://bkmk-front.vercel.app")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
