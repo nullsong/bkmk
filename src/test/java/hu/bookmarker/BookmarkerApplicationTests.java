8b4118609d21b13459398ef7f995e0a67389f3bf
null <greatnull2@gmail.com>
2025-08-14 15:36:47 +0900

    README 수정

--- src/test/java/hu/bookmarker/BookmarkerApplicationTests.java
+++ src/test/java/hu/bookmarker/BookmarkerApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import hu.bookmarker.api.book.model.BookDTO;
 import hu.bookmarker.api.book.service.BookService;
 
 @SpringBootTest
@@ -21,7 +20,5 @@ class BookmarkerApplicationTests {
 	@Test
 	@DisplayName("Service Test")
 	public void test() {
-		BookDTO dto = BookDTO.builder().keyword("토지").build();
-		ss.getBooks(dto);
 	}
 }
