17cbd21239c74a03443e1dc53cf85547a61be940
null <greatnull2@gmail.com>
2025-08-30 00:24:14 +0900

    내 별점 조회 쿼리 추가

--- src/main/java/hu/bookmarker/api/review/model/ReviewRatingDTO.java
+++ src/main/java/hu/bookmarker/api/review/model/ReviewRatingDTO.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewRatingDTO {
-    private int reviewRating;
+    private String reviewRating;
     private int count;  
 }
